37721de70cd4a64e4c9325d126c414a478d4be71
Anukarsh <anukarshr@gmail.com>
2021-10-12 22:24:35 +0530

    Update TextForm.js

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React,{useState} from "react";
+
 
 export default function TextForm(props) {
+    const [text, setText] = useState('Enter text here')
+    // setText("nakjnkldaljn")
   return (
     <div className="container">
       <div class="mb-3">
         <label htmlFor="myBox" className="form-label">
-          {props.heading}
+          {props.heading} - {text}
         </label>
         <textarea class="form-control" id="myBox" rows="9"></textarea>
       </div>
