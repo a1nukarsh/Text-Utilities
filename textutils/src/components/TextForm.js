deaef32e11554f65bf1e01566e25890d63d6adef
Anukarsh <anukarshr@gmail.com>
2021-10-12 18:59:26 +0530

    Update TextForm.js

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -1,18 +1,15 @@
 import React from "react";
 
-export default function TextForm() {
+export default function TextForm(props) {
   return (
-    <div>
-      <div class="mb-3 container">
+    <div className="container">
+      <div class="mb-3">
         <label htmlFor="myBox" className="form-label">
-          Example textarea
+          {props.heading}
         </label>
-        <textarea
-          class="form-control"
-          id="myBox"
-          rows="3"
-        ></textarea>
+        <textarea class="form-control" id="myBox" rows="9"></textarea>
       </div>
+      <button className="btn btn-primary">Convert to UPPER CASE</button>
     </div>
   );
 }
