d2c67267a5e04319bf91e7eccbd6b564e67d3a67
Anukarsh <anukarshr@gmail.com>
2021-10-13 15:22:31 +0530

    dark mode added

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,13 +1,15 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar.js";
-import TextForm from "./components/TextForm";
+// import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
       <Navbar title="Hellow" aboutText ="hello about"/>
-      <TextForm heading="Enter the text for analysis"/>
+      {/* <TextForm heading="Enter the text for analysis"/> */}
+      <About/>
     </>
   );
 }
--- textutils/src/components/Navbar.js
+++ textutils/src/components/Navbar.js
@@ -33,7 +33,7 @@ export default function Navbar(props) {
                 </a>
               </li>
             </ul>
-            <form className="d-flex">
+            {/* <form className="d-flex">
               <input
                 className="form-control me-2"
                 type="search"
@@ -43,7 +43,7 @@ export default function Navbar(props) {
               <button className="btn btn-outline-success" type="submit">
                 Search
               </button>
-            </form>
+            </form> */}
           </div>
         </div>
       </nav>
