9c5cafc968b2ee171cd787ac48cbee50168fc0e7
Anukarsh <anukarshr@gmail.com>
2021-10-22 00:54:04 +0530

    update

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,47 +1,55 @@
 // import logo from "./logo.svg";
 import "./App.css";
-// import About from "./components/About";
+import About from "./components/About";
 import React, { useState } from "react";
 import Navbar from "./components/Navbar.js";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   const [mode, setmode] = useState("light"); //Whether dark mode is enabled or not
-  const [alert, setalert] = useState(null)
-  const showAlert = (message, type)=>{
+  const [alert, setalert] = useState(null);
+  const showAlert = (message, type) => {
     setalert({
       msg: message,
-      type: type
-    })
-  }
+      type: type,
+    });
+  };
   const toggleMode = () => {
     if (mode === "light") {
       setmode("dark");
-      document.body.style.backgroundColor='black'
-      document.body.style.color='white'
-      showAlert("Dark Mode has been enabled", "success")
+      document.body.style.backgroundColor = "black";
+      document.body.style.color = "white";
+      showAlert("Dark Mode has been enabled", "success");
       document.title = "TextUtils - Dark Mode";
     } else {
       setmode("light");
-      document.body.style.backgroundColor='white'
-      document.body.style.color='black'
-      showAlert("Light Mode has been enabled", "success")
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+      showAlert("Light Mode has been enabled", "success");
       document.title = "TextUtils - Light Mode";
     }
   };
   return (
     <>
+    <Router>
       <Navbar
         title="Hellow"
         aboutText="hello about"
         mode={mode}
         toggleMode={toggleMode}
       />
-      <Alert alert={alert}/>
-      <TextForm heading="Enter the text for analysis" mode={mode}/>
-      {/* <About/> */}
-
+      <Alert alert={alert} />
+      <Switch>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/">
+          <TextForm heading="Enter the text for analysis" mode={mode} />
+        </Route>
+      </Switch>
+      </Router>
     </>
   );
 }
